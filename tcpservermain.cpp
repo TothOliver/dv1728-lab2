085cdb6b5e9022dbd2e74ecf2d50b967af09ced7
Oliver Toth <90266732+TothOliver@users.noreply.github.com>
2025-09-26 15:54:30 +0200

    tcp text server

--- tcpservermain.cpp
+++ tcpservermain.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 /* You will to add includes here */
-//Test commit
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <protocol.h>
 
 // Included to get the support library
 #include <calcLib.h>
@@ -13,6 +21,8 @@
 
 
 using namespace std;
+
+int generateTask(char* buffer, size_t bufsize);
 
 
 int main(int argc, char *argv[]){
@@ -48,5 +58,226 @@ int main(int argc, char *argv[]){
   
   printf("TCP server on: %s:%s\n", hoststring,portstring);
 
+  /*My Magic*/
+  struct addrinfo hints, *results;
+  int sockfd, bind_status;
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  int status = getaddrinfo(hoststring, portstring, &hints, &results);
+  if(status != 0 || results == NULL)
+  {
+    fprintf(stderr, "ERROR: RESOLVE ISSUE\n");
+    return EXIT_FAILURE;
+  }
+
+  for(struct addrinfo *p = results; p != NULL; p = p->ai_next){
+    sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if(sockfd == -1){
+      perror("socket");
+      continue;
+    }
+
+    int yes = 1;
+    if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1){
+      perror("setsockopt");
+      close(sockfd);
+      sockfd = -1;
+      continue;
+    }
+
+    if(bind(sockfd, p->ai_addr, p->ai_addrlen) == 0){
+      bind_status = 0;
+      break;
+    }else{
+      perror("bind");
+      close(sockfd);
+      sockfd = -1;
+    }
+  }
+  freeaddrinfo(results);
+
+  if(sockfd == -1){
+    fprintf(stderr, "ERROR: CANT CONNECT TO %d\n", sockfd);
+    return EXIT_FAILURE;
+  }
+  if(bind_status == -1){
+    freeaddrinfo(results);
+    close(sockfd);
+    perror("bind");
+    fprintf(stderr, "ERROR: CANT BIND to %d\n", sockfd);
+    return EXIT_FAILURE;
+  }
+
+  int listen_status = listen(sockfd, 5);
+  if(listen_status == -1){
+    fprintf(stderr, "ERROR: LISTEN FAILED %d\n", sockfd);
+    return EXIT_FAILURE;
+  }
+
+  struct sockaddr_storage client_addr;
+  socklen_t client_addr_size = sizeof(client_addr);
+
+  int clientfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_size);
+  if(clientfd == -1){
+      perror("accept failed");
+      return EXIT_FAILURE;
+  }
+
+  char tmsg[] = "TEXT TCP 1.1\nBINART TCP 1.1\n";
+
+  ssize_t sent = write(clientfd, tmsg, strlen(tmsg));
+  if(sent == -1){
+    freeaddrinfo(results);
+    close(sockfd);
+    fprintf(stderr, "ERROR: sendto failed\n");
+    return EXIT_FAILURE;
+  }
+  printf("Sent to client:\n%s", tmsg);
+
+  fd_set reading;
+  struct timeval timeout;
+  int rc;
+
+  char buf[1500];
+  memset(&buf, 0, sizeof(buf));
+  ssize_t byte_size;;
+    
+  FD_ZERO(&reading);
+  FD_SET(clientfd, &reading);
+  memset(&timeout, 0, sizeof(timeout));
+
+  timeout.tv_sec = 10;
+  rc = select(clientfd+1, &reading, NULL, NULL, &timeout);
+
+  if(rc > 0){
+    if(FD_ISSET(clientfd, &reading)){
+      byte_size = read(clientfd, buf, sizeof(buf));
+    }
+  }else if(rc == 0){
+    freeaddrinfo(results);
+    close(sockfd);
+    fprintf(stderr, "ERROR: TIMEOUT\n");
+    return EXIT_FAILURE;
+  }else{
+    perror("select");
+  }  
+
+  if(byte_size <= 0){
+    freeaddrinfo(results);
+    close(sockfd);
+    fprintf(stderr, "ERROR: read failed!\n");
+    return EXIT_FAILURE;
+  }
+  printf("Sent from client:\n%s", buf);
+  buf[byte_size] = '\0'; 
+
+  if((strcmp(buf, "TEXT TCP 1.1 OK\n") == 0)){
+
+    memset(&buf, 0, sizeof(buf));
+    int result = generateTask(buf, sizeof(buf));
+
+    sent = write(clientfd, buf, strlen(buf));
+    if(sent == -1){
+      freeaddrinfo(results);
+      close(sockfd);
+      fprintf(stderr, "ERROR: sendto failed\n");
+      return EXIT_FAILURE;
+    }
+    printf("Sent to client:\n%s", buf);
+
+
+    FD_ZERO(&reading);
+    FD_SET(clientfd, &reading);
+    memset(&timeout, 0, sizeof(timeout));
+
+    timeout.tv_sec = 10;
+    rc = select(clientfd+1, &reading, NULL, NULL, &timeout);
+    memset(&buf, 0, sizeof(buf));
+    if(rc > 0){
+      if(FD_ISSET(clientfd, &reading)){
+        byte_size = read(clientfd, buf, sizeof(buf));
+      }
+    }else if(rc == 0){
+      freeaddrinfo(results);
+      close(sockfd);
+      fprintf(stderr, "ERROR: TIMEOUT\n");
+      return EXIT_FAILURE;
+    }else{
+      perror("select");
+    }  
+
+    if(byte_size <= 0){
+      freeaddrinfo(results);
+      close(sockfd);
+      fprintf(stderr, "ERROR: read failed!\n");
+      return EXIT_FAILURE;
+    }
+    printf("Sent from client:\n%s", buf);
+
+    buf[byte_size] = '\0';
+    int clientResult = atoi(buf);
+    memset(&tmsg, 0, sizeof(buf));
+
+    if(clientResult == result){
+      strcpy(buf, "OK\n");
+    }
+    else{
+      strcpy(buf, "NOT OK\n");
+    }
+
+    sent = write(clientfd, buf, strlen(buf));
+    if(sent == -1){
+      freeaddrinfo(results);
+      close(sockfd);
+      fprintf(stderr, "ERROR: sendto failed\n");
+      return EXIT_FAILURE;
+    }
+    printf("Sent to client:\n%s", buf);
+
+  }
   
+  else if(strcmp(buf, "BINARY TCP 1.1 OK\n") == 0){
+
+  }
+  else{
+    fprintf(stderr, "ERROR: MISSMATCH PROTOCOL\n");
+    return EXIT_FAILURE;
+  }
+
+  printf("EXIT SERVER\n");
+  return EXIT_SUCCESS;
+}
+
+int generateTask(char* buffer, size_t bufsize){
+  char* arith = randomType();
+  int v1 = randomInt();
+  int v2 = randomInt();
+  int result;
+
+  if(strcmp(arith, "add") == 0){
+    result = v1 + v2;
+  }
+  else if(strcmp(arith, "sub") == 0){
+    result = v1 - v2;     
+  }
+  else if(strcmp(arith, "mul") == 0){
+    result = v1 * v2;
+  }
+  else if(strcmp(arith, "div") == 0){
+    if(v2 == 0)
+      v2 = 1;  
+    result = v1 / v2;
+  }
+  else{
+    fprintf(stderr, "ERROR: Invalid operation\n");
+    return EXIT_FAILURE;
+  }
+
+  snprintf(buffer, bufsize, "%s %d %d\n", arith, v1, v2);
+
+  return result;
 }
