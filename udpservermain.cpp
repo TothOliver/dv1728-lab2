b51ca90dc84bd3c16dc93df9acc6e9efba76432f
Oliver Toth <90266732+TothOliver@users.noreply.github.com>
2025-10-01 14:47:57 +0200

    final clean up

--- udpservermain.cpp
+++ udpservermain.cpp
@@ -26,11 +26,7 @@
 using namespace std;
 using Clock = std::chrono::steady_clock;
 
-
-int sendMessage(int sockfd, const void* msg, size_t msgSize, struct sockaddr_in* clientAddr, socklen_t addrLen);
-int recvMessage(int sockfd, char* buf, size_t bufsize, int timeOutSec, struct sockaddr_in* clientAddr, socklen_t* addrLen);
 int generateTask(char* buffer, size_t bufsize);
-struct addrinfo* reverseList(struct addrinfo* head);
 bool isCalcMessage(const char* buf, int byte_size);
 bool isCalcProtocol(const char* buf, int byte_size);
 
@@ -119,7 +115,6 @@ int main(int argc, char *argv[]){
       continue;
     }
 
-
     if(bind(sockfd, p->ai_addr, p->ai_addrlen) == 0){
       bind_status = 0;
       break;
@@ -149,22 +144,6 @@ int main(int argc, char *argv[]){
 
   std::unordered_map<ClientKey, ClientResult, ClientKeyHash> pending;
 
-  struct sockaddr_storage addr;
-  socklen_t len = sizeof(addr);
-  if (getsockname(sockfd, (struct sockaddr*)&addr, &len) == 0) {
-      if (addr.ss_family == AF_INET) {
-          struct sockaddr_in *sin = (struct sockaddr_in*)&addr;
-          char ip[INET_ADDRSTRLEN];
-          inet_ntop(AF_INET, &sin->sin_addr, ip, sizeof(ip));
-          printf("Bound IPv4 socket on %s:%d\n", ip, ntohs(sin->sin_port));
-      } else if (addr.ss_family == AF_INET6) {
-          struct sockaddr_in6 *sin6 = (struct sockaddr_in6*)&addr;
-          char ip[INET6_ADDRSTRLEN];
-          inet_ntop(AF_INET6, &sin6->sin6_addr, ip, sizeof(ip));
-          printf("Bound IPv6 socket on [%s]:%d\n", ip, ntohs(sin6->sin6_port));
-      }
-  }
-
   while(true){
     FD_ZERO(&reading);
     FD_SET(sockfd, &reading);
@@ -176,9 +155,9 @@ int main(int argc, char *argv[]){
     auto now = Clock::now();
     for(auto it = pending.begin(); it != pending.end();){
         if(now > it->second.deadline){
-            it = pending.erase(it);
+          it = pending.erase(it);
         }else{
-            ++it;
+          ++it;
         }
     }
 
@@ -192,19 +171,16 @@ int main(int argc, char *argv[]){
     }
 
     if(FD_ISSET(sockfd, &reading)){
-    
       char buf[1500];
       struct sockaddr_storage clientAddr;
       socklen_t addrLen = sizeof(clientAddr);
-      printf("Before recvfrom\n");
+
       ssize_t byte_size = recvfrom(sockfd, buf, sizeof(buf), 0, (struct sockaddr*)&clientAddr, &addrLen);
       if(byte_size <= 0){
         fprintf(stderr, "ERROR: read failed!\n");
         return -1;
       }
-
       printf("Bite_size %zd\n", byte_size);
-
 
       if(isCalcMessage(buf, byte_size)){
         calcProtocol cp;
@@ -262,7 +238,6 @@ int main(int argc, char *argv[]){
         cr.deadline = Clock::now() + std::chrono::seconds(10);
 
         pending[key] = cr;
-
       }
       
       else if(isCalcProtocol(buf, byte_size)){
@@ -383,185 +358,6 @@ int main(int argc, char *argv[]){
   }
 }
 
-/*
-int handleClient(int sockfd, char* buf, int byte_size, struct sockaddr_in clientAddr, socklen_t addrLen){
-      if(byte_size == sizeof(calcMessage)){ //BINARY
-        calcMessage cm;
-        memcpy(&cm, buf, sizeof(cm));
-
-        uint16_t type = ntohs(cm.type);
-        uint32_t message = ntohl(cm.message);
-        uint16_t protocol = ntohs(cm.protocol);
-        uint16_t majorv = ntohs(cm.major_version);
-        uint16_t minorv = ntohs(cm.minor_version);
-
-        if(type != 22 || message != 0 || protocol != 17 || majorv != 1 || minorv != 1){
-          fprintf(stderr, "Incorrect calcMessage\n");
-          return EXIT_FAILURE;
-        }
-        
-        calcProtocol cp;
-
-        int a, result;
-        char* arith = randomType();
-        int v1 = randomInt();
-        int v2 = randomInt();
-
-        if(v2 == 0 && strcmp(arith, "div") == 0) v2 = 1;
-        if(strcmp(arith, "add") == 0){
-          a = 1;
-          result = v1 + v2;
-        }
-        if(strcmp(arith, "sub") == 0){
-          a = 2;
-          result = v1 - v2;
-        }
-        if(strcmp(arith, "mul") == 0){
-          a = 3;
-          result = v1 * v2;
-        }
-        if(strcmp(arith, "div") == 0){
-          a = 4;
-          result = v1 / v2;
-        }
-
-        srand(time(NULL));
-        uint32_t id = rand();
-
-        cp.type = htons(1);
-        cp.major_version = htons(1);
-        cp.minor_version = htons(1);
-        cp.id = htonl(id);
-        cp.arith = htonl(a);
-        cp.inValue1 = htonl(v1);
-        cp.inValue2 = htonl(v2);
-        cp.inResult = htonl(0);
-
-        if(sendMessage(sockfd, &cp, sizeof(cp), &clientAddr, addrLen) == -1){
-          return EXIT_FAILURE;
-        }
-        printf("sendMessage calcProtocol\n");
-
-        memset(&buf, 0, sizeof(buf));
-        addrLen = sizeof(clientAddr);
-        int byte_size = recvMessage(sockfd, buf, sizeof(buf), 5, &clientAddr, &addrLen);
-        
-        if(byte_size != sizeof(calcProtocol)){
-          printf("Wrong Proctocol from recv\n");
-          return EXIT_FAILURE;
-        }
-
-        calcProtocol respons;
-        memcpy(&respons, buf, sizeof(respons));
-
-        uint16_t type2 = ntohs(respons.type);
-        uint16_t major_version = ntohs(respons.major_version);
-        uint16_t minor_version = ntohs(respons.minor_version);
-        uint32_t responsId = ntohl(respons.id);
-        int32_t inResult = ntohl(respons.inResult);
-
-        if(type2 != 2 || major_version != 1 || minor_version != 1 || responsId != id){
-          printf("type: %d, majv: %d, minv: %d, responsId: %d, id: %d\n", type, major_version, minor_version, responsId, id);
-          fprintf(stderr, "Incorrect calcProtocol\n");
-          return EXIT_FAILURE;
-        }
-
-        calcMessage reply;
-        reply.type = htons(1);
-        reply.protocol = htons(17);
-        reply.major_version = htons(1);
-        reply.minor_version = htons(1);
-
-        if(inResult == result){
-          reply.message = htonl(1);
-          printf("OK\n");
-        }
-        else{
-          reply.message = htonl(2);
-          printf("NOT OK\n");
-        }
-
-        if(sendMessage(sockfd, &reply, sizeof(reply), &clientAddr, addrLen) == -1){
-          return EXIT_FAILURE;
-        }
-
-      }
-      
-      else if(byte_size >= 12 && byte_size <= 14){ //TEXT
-        buf[byte_size] = '\0';
-        if(strcmp(buf, "TEXT UDP 1.1\n") != 0){
-          fprintf(stderr, "Incorrect TEXT message: %s", buf);
-          return EXIT_FAILURE;
-        }
-
-        memset(&buf, 0, sizeof(buf));
-        int result = generateTask(buf, sizeof(buf));
-
-        if(sendMessage(sockfd, &buf, strlen(buf), &clientAddr, addrLen) == -1){
-          return EXIT_FAILURE;
-        }
-
-        memset(&buf, 0, sizeof(buf));
-        byte_size = recvMessage(sockfd, buf, sizeof(buf), 5, &clientAddr, &addrLen);
-
-        if(byte_size <= 0){
-          perror("recvMessage");
-          return EXIT_FAILURE;
-        }
-
-        int clientResult = atoi(buf);
-        memset(&buf, 0, sizeof(buf));
-
-        if(clientResult == result){
-          printf("OK\n");
-          strcpy(buf, "OK\n");
-        }
-        else{
-          printf("NOT OK\n");
-          strcpy(buf, "NOT OK\n");
-        }
-
-        if(sendMessage(sockfd, &buf, strlen(buf), &clientAddr, addrLen) == -1){
-          return EXIT_FAILURE;
-        }
-      }
-      
-      else if(byte_size != -1){
-        printf("Wrong message byte_size: %d\n", byte_size);
-        return EXIT_FAILURE;
-      }
-      else{
-        printf("Message Invalid");
-
-      } 
-      return EXIT_SUCCESS;
-}
-*/
-
-int sendMessage(int sockfd, const void* msg, size_t msgSize, struct sockaddr_in* clientAddr, socklen_t addrLen){
-  ssize_t sent = sendto(sockfd, msg, msgSize, 0, (struct sockaddr*)clientAddr, addrLen);
-  if(sent == -1){
-    perror("sendto failed");
-    return -1;
-  }
-    return 0;
-}
-
-int recvMessage(int sockfd, char* buf, size_t bufsize, int timeOutSec, struct sockaddr_in* clientAddr, socklen_t* addrLen){
-  ssize_t byte_size = recvfrom(sockfd, buf, bufsize, 0, (struct sockaddr*)clientAddr, addrLen);
-  if(byte_size <= 0){
-    fprintf(stderr, "ERROR: read failed!\n");
-    return -1;
-  }
-
-  char ip[INET_ADDRSTRLEN];
-  inet_ntop(AF_INET, &(clientAddr->sin_addr), ip, sizeof(ip));
-  uint16_t port = ntohs(clientAddr->sin_port);
-
-  printf("Recv %zd bytes from %s:%d\n", byte_size, ip, port);
-  return byte_size;
-}
-
 int generateTask(char* buffer, size_t bufsize){
   char* arith = randomType();
   int v1 = randomInt();
@@ -590,18 +386,6 @@ int generateTask(char* buffer, size_t bufsize){
   snprintf(buffer, bufsize, "%s %d %d\n", arith, v1, v2);
 
   return result;
-}
-
-struct addrinfo* reverseList(struct addrinfo* head){
-    struct addrinfo* prev = NULL;
-    struct addrinfo* curr = head;
-    while (curr != NULL){
-        struct addrinfo* next = curr->ai_next;
-        curr->ai_next = prev;
-        prev = curr;
-        curr = next;
-    }
-    return prev;
 }
 
 bool isCalcMessage(const char* buf, int byte_size){
